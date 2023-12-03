df603157712458d62f2bf14a325772e927766015
MikosJ <56031072+MikosJ@users.noreply.github.com>
2023-12-03 20:54:55 +0100

    formatting response

--- src/main/kotlin/pk/gi/airquality/model/rest/out/AverageValues.kt
+++ src/main/kotlin/pk/gi/airquality/model/rest/out/AverageValues.kt
@@ -1,3 +1,8 @@
 package pk.gi.airquality.model.rest.out
 
-data class AverageValues(val value: Number, val parameterFormula: String, val parameterName: String, val voivodeship: String)
+data class AverageValues(
+    val value: Number,
+    val parameterFormula: String,
+    val parameterName: String,
+    val voivodeship: String
+)
--- src/main/kotlin/pk/gi/airquality/model/rest/out/GraphValues.kt
+++ src/main/kotlin/pk/gi/airquality/model/rest/out/GraphValues.kt
@@ -1,9 +1,11 @@
 package pk.gi.airquality.model.rest.out
 
+import com.fasterxml.jackson.annotation.JsonFormat
 import java.time.LocalDateTime
 
 data class GraphValues(
     val value: Number,
+    @JsonFormat(pattern="yyyy-MM-dd HH:mm")
     val date: LocalDateTime,
     val parameterFormula: String,
     val parameterName: String,
